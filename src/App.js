40e87a3e66410b0f413662de88e69080d9252bfa
Jesse Ginnever <jesseginnever@gmail.com>
2018-02-08 23:37:40 -0600

    Create columns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, Jumbotron, Button, MenuItem, Nav, NavItem, NavDropdown, Panel, Well} from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, MenuItem, Nav, NavItem, NavDropdown, Panel, Well, Grid, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -32,17 +32,22 @@ class App extends Component {
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
+
+        //Main content
         <div className="Main-content">
-            <p className="App-intro">
-                <Well>
-                      To get started, edit<code>src/App.js</code> and save to reload.
-                </Well>
-            </p>
-            <Panel>
-                <Panel.Heading>Panel heading without a title</Panel.Heading>
-                <Panel.Body>Panel content</Panel.Body>
-              </Panel>
-            <Button bsStyle="primary">Primary</Button>
+            <Col xs={12} md={8}>
+                <p className="App-intro">
+                    <Well>
+                          To get started, edit<code>src/App.js</code> and save to reload.
+                    </Well>
+                </p>
+            </Col>
+            <Col xs={6} md={4}>
+                <Panel>
+                    <Panel.Heading>Panel heading without a title</Panel.Heading>
+                    <Panel.Body>Panel content</Panel.Body>
+                </Panel>
+            </Col>
         </div>
       </div>
     );
